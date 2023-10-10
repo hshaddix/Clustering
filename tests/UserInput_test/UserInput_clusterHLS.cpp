e5043752bf615f37554407d085720e079cbe62cc
Hayden Shaddix <hshaddix@login02.af.uchicago.edu>
2023-10-10 09:01:52 -0500

    10/10/2023 - Updated HLS code

--- tests/UserInput_test/UserInput_clusterHLS.cpp
+++ tests/UserInput_test/UserInput_clusterHLS.cpp
@@ -1,31 +1,23 @@
 #include <ap_int.h>
 
-void processGroups(ap_int<32> inputVector[100], ap_int<32> groupInfo[100][2]) {
-  ap_int<32> groupStart = 0;
-  ap_int<32> groupSum = 0;
-  ap_int<32> groupSize = 0;
-
-  for (int i = 1; i <= 100; i++) {
-    if (i == 100 || inputVector[i] != inputVector[i-1] + 1) {
-      groupSize = i - groupStart;
-
 #define SIZE 100
 
 void processGroups(int inputVector[SIZE], int groupInfo[SIZE][2], int size) {
   int groupStart = 0;
+  int groupSum = 0; // Added declaration for groupSum
+
   for (int i = 1; i <= size; i++) {
-        #pragma HLS PIPELINE
+    #pragma HLS PIPELINE
     if (i == size || inputVector[i] != inputVector[i-1] + 1) {
       int groupSize = i - groupStart;
       if (groupSize == 1) {
-	groupInfo[groupStart][0] = inputVector[groupStart];
-	groupInfo[groupStart][1] = 1;
+        groupInfo[groupStart][0] = inputVector[groupStart];
+        groupInfo[groupStart][1] = 1;
       } else if (groupSize >= 2) {
-	groupSum = inputVector[groupStart];
-	groupSum += inputVector[i-1];
-	groupInfo[groupStart][0] = groupSum;
-	groupInfo[groupStart][0] = inputVector[groupStart] + inputVector[i-1];
-	groupInfo[groupStart][1] = inputVector[i-1] - inputVector[groupStart] + 1;
+        groupSum = inputVector[groupStart]; // Store the start of the group
+        groupSum += inputVector[i-1];      // Accumulate the group sum
+        groupInfo[groupStart][0] = groupSum;
+        groupInfo[groupStart][1] = inputVector[i-1] - inputVector[groupStart] + 1;
       }
       groupStart = i;
     }

a31017fa13c1aea4c6a866a40574ef063dbb0757
Hayden Shaddix <hshaddix@login02.af.uchicago.edu>
2023-10-10 07:56:08 -0500

    10/10/2023 - Updated HLS file includes both the sum and the difference of input vector group

---
+++ tests/UserInput_test/UserInput_clusterHLS.cpp
@@ -0,0 +1,23 @@
+#include <ap_int.h>
+
+void processGroups(ap_int<32> inputVector[100], ap_int<32> groupInfo[100][2]) {
+  ap_int<32> groupStart = 0;
+  ap_int<32> groupSum = 0;
+  ap_int<32> groupSize = 0;
+
+  for (int i = 1; i <= 100; i++) {
+    if (i == 100 || inputVector[i] != inputVector[i-1] + 1) {
+      groupSize = i - groupStart;
+      if (groupSize == 1) {
+	groupInfo[groupStart][0] = inputVector[groupStart];
+	groupInfo[groupStart][1] = 1;
+      } else if (groupSize >= 2) {
+	groupSum = inputVector[groupStart];
+	groupSum += inputVector[i-1];
+	groupInfo[groupStart][0] = groupSum;
+	groupInfo[groupStart][1] = inputVector[i-1] - inputVector[groupStart] + 1;
+      }
+      groupStart = i;
+    }
+  }
+}

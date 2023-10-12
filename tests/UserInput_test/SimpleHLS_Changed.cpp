5c8f0bb98de72ad911127f9c72765c1ef6be2c44
Hayden Shaddix <hshaddix@login02.af.uchicago.edu>
2023-10-11 19:41:03 -0500

    10/11/2023 - Trying further changes to original to fix some warning signals from vitis

--- tests/UserInput_test/SimpleHLS_Changed.cpp
+++ tests/UserInput_test/SimpleHLS_Changed.cpp
@@ -4,26 +4,49 @@
 
 void processGroups(int inputVector[SIZE], int groupInfo[SIZE][2], int size) {
     #pragma HLS PIPELINE II=2
+    #pragma HLS LOOP_TRIPCOUNT min=1 max=100
+
+  if (size == 1) {
+    groupInfo[0][0] = inputVector[0];
+    groupInfo[0][1] = 1;
+    return;
+  }
 
   int groupStart = 0;
   int prevValue = inputVector[0];
 
-  for (int i = 1; i < size; i++) {
-    int currentValue = inputVector[i];
+  for (int i = 1; i < size; i+=2) {
+    int currentValue1 = inputVector[i];
+    int currentValue2 = (i+1 < size) ? inputVector[i+1] : currentValue1;
 
-    if (currentValue != prevValue + 1) {
+    if (currentValue1 != prevValue + 1) {
       int groupSize = i - groupStart;
 
       if (groupSize == 1) {
 	groupInfo[groupStart][0] = prevValue;
 	groupInfo[groupStart][1] = 1;
       } else if (groupSize >= 2) {
-	groupInfo[groupStart][0] = prevValue + currentValue;
-	groupInfo[groupStart][1] = currentValue - prevValue + 1;
+	groupInfo[groupStart][0] = prevValue + currentValue1;
+	groupInfo[groupStart][1] = currentValue1 - prevValue + 1;
       }
 
       groupStart = i;
-      prevValue = currentValue;
+      prevValue = currentValue1;
+    }
+
+    if (currentValue2 != currentValue1 + 1) {
+      int groupSize = i+1 - groupStart;
+
+      if (groupSize == 1) {
+	groupInfo[groupStart][0] = currentValue1;
+	groupInfo[groupStart][1] = 1;
+      } else if (groupSize >= 2) {
+	groupInfo[groupStart][0] = currentValue1 + currentValue2;
+	groupInfo[groupStart][1] = currentValue2 - currentValue1 + 1;
+      }
+
+      groupStart = i+1;
+      prevValue = currentValue2;
     }
   }
 }

8fffed587ccabab7cbd315490bafacd16778a374
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2023-11-30 17:34:23 -0600

    Update bitCluster.cpp

--- tests/UserInput_test/bitCluster.cpp
+++ tests/UserInput_test/bitCluster.cpp
@@ -3,50 +3,35 @@
 #include <bitset>
 #include <string>
 
-const int EMPTY_CLUSTER_SET = 0;
-const int EMPTY_CLUSTER_SET_ERROR_CODE = 0xFE;
 const int STRIP_SIZE = 126;
+const int HIT_ADDRESS_BITS = 8;
 
 std::pair<int, int> calculate_if_from_sum_size(int sum, int size) {
-    if (sum == 0 && size == 0) {
-        return {EMPTY_CLUSTER_SET_ERROR_CODE, EMPTY_CLUSTER_SET_ERROR_CODE};
-    }
     int f = (sum + size - 1) / 2;
     int i = (sum - size + 1) / 2;
     return {i, f};
 }
 
-bool are_adjacent(const std::pair<int, int>& cluster1, const std::pair<int, int>& cluster2, int offset) {
-    int cluster2_start_with_offset = cluster2.first + offset * STRIP_SIZE;
-    return cluster1.second == cluster2_start_with_offset - 1;
+bool are_adjacent(const std::pair<int, int>& cluster1, const std::pair<int, int>& cluster2) {
+    return cluster1.second == cluster2.first - 1;
 }
 
 std::vector<std::pair<int, int>> merge_clusters(const std::vector<std::pair<int, int>>& clusters) {
     std::vector<std::pair<int, int>> merged;
     int offset = 0;
-    int previous_f = -1;
 
     for (size_t i = 0; i < clusters.size(); ++i) {
-        if (clusters[i].first == EMPTY_CLUSTER_SET_ERROR_CODE) {
-            std::cout << "FE "; // Output FE for empty strip
-            offset++;
-            continue;
-        }
-
         int current_i = clusters[i].first + offset * STRIP_SIZE;
         int current_f = clusters[i].second + offset * STRIP_SIZE;
 
-        if (previous_f > current_i) {
-            offset++;
-            current_i += STRIP_SIZE;
-            current_f += STRIP_SIZE;
-        }
-        previous_f = current_f;
-
-        if (!merged.empty() && are_adjacent(merged.back(), {current_i, current_f}, 0)) {
+        if (!merged.empty() && are_adjacent(merged.back(), {current_i, current_f})) {
             merged.back().second = current_f;
         } else {
             merged.push_back({current_i, current_f});
+        }
+
+        if (i + 1 < clusters.size() && clusters[i + 1].first == 0) {
+            offset++;
         }
     }
 
@@ -62,22 +47,18 @@ int main() {
         std::bitset<8> binary_sum(binary_input.substr(0, 8));
         int sum = static_cast<int>(binary_sum.to_ulong());
         int size = 2;  // Assuming size is always 2
-        bool is_last_cluster = binary_input[8] == '1';
-        
         auto cluster = calculate_if_from_sum_size(sum, size);
-        if (cluster.first != EMPTY_CLUSTER_SET_ERROR_CODE) {
-            clusters.push_back(cluster);
-        }
-
-        // Handle last cluster in the strip
-        if (is_last_cluster) {
+        clusters.push_back(cluster);
+        if (binary_input[8] == '1') {  // Last cluster in the strip
             auto merged_clusters = merge_clusters(clusters);
             clusters.clear();
 
             for (const auto& cluster : merged_clusters) {
-                std::bitset<8> binary_sum(cluster.first + cluster.second);
+                int sum = cluster.first + cluster.second;
                 int size = cluster.second - cluster.first + 1;
-                std::cout << "{" << binary_sum.to_string() << "," << std::bitset<8>(size).to_string() << "} ";
+                std::bitset<8> binary_sum_output(sum);
+                std::bitset<8> binary_size_output(size);
+                std::cout << "{" << binary_sum_output.to_string() << "," << binary_size_output.to_string() << "} ";
             }
             std::cout << std::endl;
         }

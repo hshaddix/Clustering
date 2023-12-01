15dc370059f3b2049a86b78b95f19979757aeef9
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2023-12-01 09:13:16 -0600

    Update bitCluster.cpp

--- tests/UserInput_test/bitCluster.cpp
+++ tests/UserInput_test/bitCluster.cpp
@@ -11,10 +11,6 @@ std::pair<int, int> calculate_if_from_sum_size(int sum, int size) {
     return {i, f};
 }
 
-bool are_adjacent(int f1, int i2) {
-    return f1 + 1 == i2;
-}
-
 std::vector<std::pair<int, int>> merge_clusters(const std::vector<std::pair<int, int>>& clusters) {
     std::vector<std::pair<int, int>> merged;
     int offset = 0;
@@ -23,23 +19,18 @@ std::vector<std::pair<int, int>> merge_clusters(const std::vector<std::pair<int,
         int current_i = clusters[i].first + offset * STRIP_SIZE;
         int current_f = clusters[i].second + offset * STRIP_SIZE;
 
-        if (!merged.empty() && are_adjacent(merged.back().second, current_i)) {
-            merged.back().second = current_f;
+        if (!merged.empty() && merged.back().second == current_i - 1) {
+            merged.back().second = current_f; // Merge with previous cluster
         } else {
             merged.push_back({current_i, current_f});
         }
 
         if (clusters[i].second == -1 || (i < clusters.size() - 1 && clusters[i + 1].first == -1)) {
-            offset++;
+            offset++; // Increment offset at the end of a strip
         }
     }
 
     return merged;
-}
-
-std::string format_binary_output(int num) {
-    std::bitset<8> binary(num);
-    return binary.to_string();
 }
 
 int main() {
@@ -51,15 +42,11 @@ int main() {
         std::bitset<8> binary_size(binary_input.substr(8, 8));
         bool is_last_in_strip = binary_input[16] == '1';
 
-        if (binary_sum.to_ulong() == 0 && binary_size.to_ulong() == 0) {
-            clusters.push_back({-1, -1}); // Empty strip indicator
-            continue;
-        }
+        auto cluster = calculate_if_from_sum_size(binary_sum.to_ulong(), binary_size.to_ulong());
+        clusters.push_back(cluster);
 
-        clusters.push_back(calculate_if_from_sum_size(binary_sum.to_ulong(), binary_size.to_ulong()));
-
-        if (is_last_in_strip) {
-            clusters.push_back({-1, -1}); // Mark end of strip
+        if (is_last_in_strip || binary_sum.to_ulong() == 0 && binary_size.to_ulong() == 0) {
+            clusters.push_back({-1, -1}); // Indicate end of strip
         }
     }
 
@@ -69,9 +56,9 @@ int main() {
         if (cluster.first != -1) {
             int sum = cluster.first + cluster.second;
             int size = cluster.second - cluster.first + 1;
-            std::cout << "{" << format_binary_output(sum) << "," << format_binary_output(size) << "} ";
-        } else {
-            std::cout << "FE ";
+            std::bitset<8> binary_sum(sum);
+            std::bitset<8> binary_size(size);
+            std::cout << "{" << binary_sum.to_string() << "," << binary_size.to_string() << "} ";
         }
     }
     std::cout << std::endl;

3947cef8cf15f8c16f10eae3889116fd88437c2d
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2023-12-01 09:27:28 -0600

    Update bitCluster.cpp

--- tests/UserInput_test/bitCluster.cpp
+++ tests/UserInput_test/bitCluster.cpp
@@ -16,6 +16,15 @@ std::vector<std::pair<int, int>> merge_clusters(const std::vector<std::pair<int,
     int offset = 0;
 
     for (size_t i = 0; i < clusters.size(); ++i) {
+        if (clusters[i].first == -1 && clusters[i].second == -1) {
+            // Handle empty strip
+            if (merged.empty() || merged.back().first != -1) {
+                merged.push_back({-1, -1});
+            }
+            offset++;
+            continue;
+        }
+
         int current_i = clusters[i].first + offset * STRIP_SIZE;
         int current_f = clusters[i].second + offset * STRIP_SIZE;
 
@@ -23,10 +32,6 @@ std::vector<std::pair<int, int>> merge_clusters(const std::vector<std::pair<int,
             merged.back().second = current_f; // Merge with previous cluster
         } else {
             merged.push_back({current_i, current_f});
-        }
-
-        if (clusters[i].second == -1 || (i < clusters.size() - 1 && clusters[i + 1].first == -1)) {
-            offset++; // Increment offset at the end of a strip
         }
     }
 
@@ -38,28 +43,35 @@ int main() {
     std::string binary_input;
 
     while (std::cin >> binary_input) {
+        if (binary_input == "00000000000000001") {
+            // Handle empty strip
+            clusters.push_back({-1, -1});
+            continue;
+        }
+
         std::bitset<8> binary_sum(binary_input.substr(0, 8));
         std::bitset<8> binary_size(binary_input.substr(8, 8));
-        bool is_last_in_strip = binary_input[16] == '1';
-
         auto cluster = calculate_if_from_sum_size(binary_sum.to_ulong(), binary_size.to_ulong());
         clusters.push_back(cluster);
 
-        if (is_last_in_strip || (binary_sum.to_ulong() == 0 && binary_size.to_ulong() == 0)) {
-    clusters.push_back({-1, -1}); // Indicate end of strip
-}
+        if (binary_input[16] == '1') {
+            clusters.push_back({-1, -1}); // Indicate end of strip
+        }
     }
 
     auto merged_clusters = merge_clusters(clusters);
 
     for (const auto& cluster : merged_clusters) {
-        if (cluster.first != -1) {
-            int sum = cluster.first + cluster.second;
-            int size = cluster.second - cluster.first + 1;
-            std::bitset<8> binary_sum(sum);
-            std::bitset<8> binary_size(size);
-            std::cout << "{" << binary_sum.to_string() << "," << binary_size.to_string() << "} ";
+        if (cluster.first == -1 && cluster.second == -1) {
+            std::cout << "FE ";
+            continue;
         }
+
+        int sum = cluster.first + cluster.second;
+        int size = cluster.second - cluster.first + 1;
+        std::bitset<8> binary_sum(sum);
+        std::bitset<8> binary_size(size);
+        std::cout << "{" << binary_sum.to_string() << "," << binary_size.to_string() << "} ";
     }
     std::cout << std::endl;
 

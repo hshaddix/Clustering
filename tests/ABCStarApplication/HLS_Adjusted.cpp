baf618e16f2422bcbbcc9d39abb18bfca9b1d216
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-03-20 13:09:47 -0500

    Update HLS_Adjusted.cpp

--- tests/ABCStarApplication/HLS_Adjusted.cpp
+++ tests/ABCStarApplication/HLS_Adjusted.cpp
@@ -3,7 +3,6 @@
 
 #define MAX_HITS 1024
 #define MAX_CLUSTERS 128
-#define STRIP_SIZE 126
 #define MODULE_NUMBER_BITS 11
 #define POSITION_BITS 8
 
@@ -18,38 +17,39 @@ void processHits(ap_uint<16> inputBinaries[MAX_HITS], int inputHitCount, Hit out
     #pragma HLS INTERFACE s_axilite port=outputClusterCount
     #pragma HLS INTERFACE m_axi depth=MAX_HITS port=inputBinaries
     #pragma HLS INTERFACE m_axi depth=MAX_CLUSTERS port=outputClusters
-    #pragma HLS DATAFLOW
-
-    hls::stream<Hit> hitStream;
-    #pragma HLS STREAM variable=hitStream depth=256
-
-    // Decode and stream hits
-    for (int i = 0; i < inputHitCount; ++i) {
-        #pragma HLS PIPELINE II=1
-        ap_uint<MODULE_NUMBER_BITS> moduleNumber = inputBinaries[i] >> (16 - MODULE_NUMBER_BITS);
-        ap_uint<POSITION_BITS> seedPosition = (inputBinaries[i] & ((1 << POSITION_BITS) - 1));
-        ap_uint<3> sizeBitmask = (inputBinaries[i] >> POSITION_BITS) & 0x7;
-
-        if (sizeBitmask[0]) hitStream.write({moduleNumber, seedPosition + 1});
-        if (sizeBitmask[1]) hitStream.write({moduleNumber, seedPosition + 2});
-        if (sizeBitmask[2]) hitStream.write({moduleNumber, seedPosition + 3});
-    }
 
     outputClusterCount = 0;
 
-    // Process streamed hits to form clusters
-    Hit previousHit = {0, 0};
-    for (int i = 0; i < MAX_HITS && !hitStream.empty(); ++i) {
-        #pragma HLS PIPELINE II=1
-        Hit currentHit;
-        hitStream.read(currentHit);
-        bool isNewCluster = (i == 0) || !(currentHit.moduleNumber == previousHit.moduleNumber && currentHit.position == previousHit.position + 1);
+    ap_uint<MODULE_NUMBER_BITS> lastModuleNumber = 0;
+    ap_uint<POSITION_BITS> lastPosition = 0;
+    bool firstHit = true;
 
-        if (isNewCluster) {
-            if (outputClusterCount < MAX_CLUSTERS) {
-                outputClusters[outputClusterCount++] = currentHit;
+    for (int i = 0; i < inputHitCount && outputClusterCount < MAX_CLUSTERS; ++i) {
+        #pragma HLS PIPELINE II=1
+        ap_uint<16> inputBinary = inputBinaries[i];
+        ap_uint<MODULE_NUMBER_BITS> moduleNumber = inputBinary >> (16 - MODULE_NUMBER_BITS);
+        ap_uint<POSITION_BITS> seedPosition = (inputBinary & ((1 << POSITION_BITS) - 1));
+        ap_uint<3> sizeBitmask = (inputBinary >> POSITION_BITS) & 0x7;
+
+        for (ap_uint<2> j = 0; j < 3; j++) {
+            if (sizeBitmask[j]) {
+                ap_uint<POSITION_BITS> position = seedPosition + j + 1; // Correctly accounting for cluster size
+                bool isNewCluster = firstHit || !(moduleNumber == lastModuleNumber && position == lastPosition + 1);
+
+                if (isNewCluster) {
+                    if (!firstHit) outputClusterCount++;
+                    if (outputClusterCount < MAX_CLUSTERS) {
+                        outputClusters[outputClusterCount].moduleNumber = moduleNumber;
+                        outputClusters[outputClusterCount].position = position;
+                    }
+                    firstHit = false;
+                }
+
+                lastModuleNumber = moduleNumber;
+                lastPosition = position;
             }
         }
-        previousHit = currentHit;
     }
+
+    if (!firstHit) outputClusterCount++; // Account for the last cluster
 }

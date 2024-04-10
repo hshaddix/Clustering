063481c2a5ca319fea7fd04eace90661fc946e1a
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-10 13:08:02 -0500

    Create processHits.h

---
+++ tests/ABCStarApplication/testing/processHits.h
@@ -0,0 +1,30 @@
+// processHits.h
+#ifndef PROCESS_HITS_H
+#define PROCESS_HITS_H
+
+#include <ap_int.h>
+#include <hls_stream.h>
+
+#define MAX_HITS 1024
+#define MAX_CLUSTERS 127
+#define ABCStar_ID_BITS 11
+#define POSITION_BITS 8
+#define ABCStar_SIZE 128
+
+struct Hit {
+    ap_uint<ABCStar_ID_BITS> ABCStarID;
+    ap_uint<POSITION_BITS> position;
+};
+
+struct ClusterInfo {
+    Hit firstHit;
+    int size;
+};
+
+struct InputData {
+    ap_uint<16> data;
+};
+
+void processHits(hls::stream<InputData>& inputBinariesStream, int inputHitCount, ClusterInfo outputClusters[MAX_CLUSTERS], int& outputClusterCount);
+
+#endif

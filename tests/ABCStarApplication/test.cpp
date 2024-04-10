638846b8fa0109b101f799463b36e9020858c56d
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-10 10:09:42 -0500

    Update test.cpp

--- tests/ABCStarApplication/test.cpp
+++ tests/ABCStarApplication/test.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <ap_int.h>
 #include <hls_stream.h>
-#include <vector>
 
 #define MAX_HITS 1024
 #define MAX_CLUSTERS 127
@@ -133,42 +131,3 @@ void processHits(hls::stream<InputData> &inputBinariesStream, int inputHitCount,
         outputClusterCount = MAX_CLUSTERS;
     }
 }
-
-#ifndef __SYNTHESIS__
-// Testbench
-int main() {
-    hls::stream<InputData> testDataStream;
-    ClusterInfo testOutputClusters[MAX_CLUSTERS];
-    int testOutputClusterCount = 0;
-
-    // Case 1
-    InputData inputDataCase1 = {0b000000110010010};
-    testDataStream.write(inputDataCase1);
-    processHits(testDataStream, 1, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 1 Output Cluster Count: " << testOutputClusterCount << std::endl;
-
-    // Reset stream for the next case
-    testDataStream = {};
-
-    // Case 2
-    InputData inputDataCase2_1 = {0b000000110010101};
-    InputData inputDataCase2_2 = {0b000000110110100};
-    testDataStream.write(inputDataCase2_1);
-    testDataStream.write(inputDataCase2_2);
-    processHits(testDataStream, 2, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 2 Output Cluster Count: " << testOutputClusterCount << std::endl;
-
-    // Reset stream for the next case
-    testDataStream = {};
-
-    // Case 3
-    InputData inputDataCase3_1 = {0b0000001111010111};
-    InputData inputDataCase3_2 = {0b0000100000000110};
-    testDataStream.write(inputDataCase3_1);
-    testDataStream.write(inputDataCase3_2);
-    processHits(testDataStream, 2, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 3 Output Cluster Count: " << testOutputClusterCount << std::endl;
-
-    return 0;
-}
-#endif

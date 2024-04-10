8e9fcceb8ead9ac5ea7e379ea571c274254b4e30
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-10 12:30:36 -0500

    Update testBench.cpp

--- tests/ABCStarApplication/testBench.cpp
+++ tests/ABCStarApplication/testBench.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
-#include "test.cpp" // Include the core functionality
+#include "test.cpp" // Make sure this path is correctly set to where processHits.cpp is located
 
-#ifndef __SYNTHESIS__
-// Testbench
 int main() {
-    hls::stream<InputData> testDataStream;
     ClusterInfo testOutputClusters[MAX_CLUSTERS];
-    int testOutputClusterCount = 0;
+    int testOutputClusterCount;
 
     // Case 1
-    InputData inputDataCase1 = {0b000000110010010};
-    testDataStream.write(inputDataCase1);
-    processHits(testDataStream, 1, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 1 Output Cluster Count: " << testOutputClusterCount << std::endl;
-
-    // Reset stream for the next case
-    testDataStream = {};
+    std::cout << "Running case 1:" << std::endl;
+    {
+        hls::stream<InputData> testDataStream;
+        testOutputClusterCount = 0;
+        InputData inputDataCase1 = {0b000000110010010};
+        testDataStream.write(inputDataCase1);
+        processHits(testDataStream, 1, testOutputClusters, &testOutputClusterCount);
+        std::cout << "Case 1 Output Cluster Count: " << testOutputClusterCount << std::endl;
+    }
 
     // Case 2
-    InputData inputDataCase2_1 = {0b000000110010101};
-    InputData inputDataCase2_2 = {0b000000110110100};
-    testDataStream.write(inputDataCase2_1);
-    testDataStream.write(inputDataCase2_2);
-    processHits(testDataStream, 2, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 2 Output Cluster Count: " << testOutputClusterCount << std::endl;
-
-    // Reset stream for the next case
-    testDataStream = {};
+    std::cout << "\nRunning case 2:" << std::endl;
+    {
+        hls::stream<InputData> testDataStream;
+        testOutputClusterCount = 0;
+        InputData inputDataCase2_1 = {0b000000110010101};
+        InputData inputDataCase2_2 = {0b000000110110100};
+        testDataStream.write(inputDataCase2_1);
+        testDataStream.write(inputDataCase2_2);
+        processHits(testDataStream, 2, testOutputClusters, &testOutputClusterCount);
+        std::cout << "Case 2 Output Cluster Count: " << testOutputClusterCount << std::endl;
+    }
 
     // Case 3
-    InputData inputDataCase3_1 = {0b0000001111010111};
-    InputData inputDataCase3_2 = {0b0000100000000110};
-    testDataStream.write(inputDataCase3_1);
-    testDataStream.write(inputDataCase3_2);
-    processHits(testDataStream, 2, testOutputClusters, testOutputClusterCount);
-    std::cout << "Case 3 Output Cluster Count: " << testOutputClusterCount << std::endl;
+    std::cout << "\nRunning case 3:" << std::endl;
+    {
+        hls::stream<InputData> testDataStream;
+        testOutputClusterCount = 0;
+        InputData inputDataCase3_1 = {0b0000001111010111};
+        InputData inputDataCase3_2 = {0b0000100000000110};
+        testDataStream.write(inputDataCase3_1);
+        testDataStream.write(inputDataCase3_2);
+        processHits(testDataStream, 2, testOutputClusters, &testOutputClusterCount);
+        std::cout << "Case 3 Output Cluster Count: " << testOutputClusterCount << std::endl;
+    }
 
     return 0;
 }
-#endif

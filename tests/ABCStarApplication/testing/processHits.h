2ed82266bc2869e42acfba7328cb3695a05fab95
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-17 12:35:19 -0500

    Update processHits.h

--- tests/ABCStarApplication/testing/processHits.h
+++ tests/ABCStarApplication/testing/processHits.h
@@ -1,29 +1,25 @@
 // processHits.h
-#ifndef PROCESS_HITS_H
-#define PROCESS_HITS_H
-
 #include <ap_int.h>
 #include <hls_stream.h>
+#include <ap_axi_sdata.h>
 
-#define MAX_HITS 1024
 #define MAX_CLUSTERS 127
 #define ABCStar_ID_BITS 5
 #define POSITION_BITS 8
-#define ABCStar_SIZE 128
+#define ABCStar_SIZE 128 // Assuming a fixed ABCStar size of 128 positions.
 
 struct Hit {
     ap_uint<ABCStar_ID_BITS> ABCStarID;
     ap_uint<POSITION_BITS> position;
 };
 
-struct ClusterInfo {
-    Hit firstHit;
-    int size;
+struct OutputClusterInfo {
+    ap_uint<POSITION_BITS> position;  // Position of the seed hit
+    ap_uint<4> size;                  // Size of the cluster, up to 15
 };
 
-struct InputData {
-    ap_uint<16> data;
-};
+typedef ap_axiu<16, 0, 0, 1> InputData;  // 16-bit data
+typedef ap_axiu<12, 0, 0, 1> OutputData; // 12-bit data (8 bits for position + 4 bits for size)
 
 void processHits(hls::stream<InputData>& inputBinariesStream, int inputHitCount, ClusterInfo outputClusters[MAX_CLUSTERS], int& outputClusterCount);
 

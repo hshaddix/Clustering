551cb1bce8864f4b3eda20dc2027e19088bd0eb2
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-21 14:37:03 -0500

    Update processHits.h
    changed header to fit formatting

--- tests/ABCStarApplication/testing/processHits.h
+++ tests/ABCStarApplication/testing/processHits.h
@@ -3,9 +3,10 @@
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
-#define MAX_CLUSTERS 127
+// #define MAX_CLUSTERS 127
 #define ABCStar_ID_BITS 5
 #define POSITION_BITS 8
+#define SIZE_BITS 3
 #define ABCStar_SIZE 128 // Assuming a fixed ABCStar size of 128 positions.
 
 struct Hit {
@@ -14,13 +15,16 @@ struct Hit {
 };
 
 struct OutputClusterInfo {
+    ap_uint<ABCStar_ID_BITS> ABCStarID; // ABCStarID of the cluster
     ap_uint<POSITION_BITS> position;  // Position of the seed hit
     ap_uint<4> size;                  // Size of the cluster, up to 15
 };
 
 typedef ap_axiu<16, 0, 0, 1> InputData;  // 16-bit data
-typedef ap_axiu<12, 0, 0, 1> OutputData; // 12-bit data (8 bits for position + 4 bits for size)
 
-void processHits(hls::stream<InputData>& inputBinariesStream, int inputHitCount, ClusterInfo outputClusters[MAX_CLUSTERS], int& outputClusterCount);
+// Define a new struct for output data to encapsulate it in a stream
+typedef ap_axiu<(ABCStar_ID_BITS + POSITION_BITS + SIZE_BITS), 0, 0, 1> OutputData; // Data width sum of parts
+
+void processHits(hls::stream<InputData> &inputBinariesStream, hls::stream<OutputData> &outputClustersStream);
 
 #endif

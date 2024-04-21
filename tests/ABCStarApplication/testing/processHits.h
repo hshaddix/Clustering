42be4e3c0d60fd42ad8b9a9a90eba9e163dd2094
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-04-21 14:55:00 -0500

    Update processHits.h

--- tests/ABCStarApplication/testing/processHits.h
+++ tests/ABCStarApplication/testing/processHits.h
@@ -1,30 +1,24 @@
 // processHits.h
+#ifndef PROCESS_HITS_H
+#define PROCESS_HITS_H
+
 #include <ap_int.h>
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
-// #define MAX_CLUSTERS 127
 #define ABCStar_ID_BITS 5
 #define POSITION_BITS 8
 #define SIZE_BITS 3
-#define ABCStar_SIZE 128 // Assuming a fixed ABCStar size of 128 positions.
+#define ABCStar_SIZE 128
 
 struct Hit {
     ap_uint<ABCStar_ID_BITS> ABCStarID;
     ap_uint<POSITION_BITS> position;
 };
 
-struct OutputClusterInfo {
-    ap_uint<ABCStar_ID_BITS> ABCStarID; // ABCStarID of the cluster
-    ap_uint<POSITION_BITS> position;  // Position of the seed hit
-    ap_uint<SIZE_BITS> size;  // Size of the cluster, up to 7
-};
-
 typedef ap_axiu<16, 0, 0, 1> InputData;  // 16-bit data
-
-// Define a new struct for output data to encapsulate it in a stream
 typedef ap_axiu<(ABCStar_ID_BITS + POSITION_BITS + SIZE_BITS), 0, 0, 1> OutputData; // Data width sum of parts
 
 void processHits(hls::stream<InputData> &inputBinariesStream, hls::stream<OutputData> &outputClustersStream);
 
-#endif
+#endif // PROCESS_HITS_H

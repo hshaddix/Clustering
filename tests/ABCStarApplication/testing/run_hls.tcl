e566eff52a3837db8017235d234c5980db0e46d8
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-06-30 23:09:05 -0500

    Update run_hls.tcl
    Make type file in the format of the tutorial

--- tests/ABCStarApplication/testing/run_hls.tcl
+++ tests/ABCStarApplication/testing/run_hls.tcl
@@ -1,12 +1,46 @@
+
+# Create a project
 open_project -reset processHits
-set_top processHits
+
+# Add design files
 add_files processHits.cpp
+# Add test bench & files
 add_files -tb testBench.cpp
-open_solution solution1
-set_part {xcvu9p-flga2104-2-e}
-create_clock -period 5 -name default
+
+# Set the top-level function
+set_top processHits
+
+# ########################################################
+# Create a solution
+open_solution -reset solution1
+# Define technology and clock rate
+set_part  {xcvu9p-flga2104-2-e}
+create_clock -period 5
+
+# Set variable to select which steps to execute
+set hls_exec 2
+
+# Run C simulation
 csim_design
-csynth_design
-cosim_design -rtl
-export_design -format ip_catalog
+
+if {$hls_exec == 1} {
+    # Run Synthesis and Exit
+    csynth_design
+    
+} elseif {$hls_exec == 2} {
+    # Run Synthesis, RTL Simulation and Exit
+    csynth_design
+    
+    cosim_design
+} elseif {$hls_exec == 3} { 
+    # Run Synthesis, RTL Simulation, RTL implementation and Exit
+    csynth_design
+    
+    cosim_design
+    export_design -format ip_catalog -xo processHits.xo
+} else {
+    # Default is to exit after setup
+    csynth_design
+}
+
 exit

332275765cad15454ec35f637b27d04c1f98581d
Hayden Shaddix <92125223+hshaddix@users.noreply.github.com>
2024-08-02 08:00:30 -0500

    Update host.cpp
    Attempted kernel with function setupDevice

--- tests/ABCStarApplication/Vitis_HLS_Kernel/src/host.cpp
+++ tests/ABCStarApplication/Vitis_HLS_Kernel/src/host.cpp
@@ -3,7 +3,30 @@
 #include <iostream>
 #include <bitset>
 
+// Define the data size
 #define DATA_SIZE 4096
+
+// Function to set up OpenCL device
+int setupDevice(std::vector<cl::Device>& devices, cl::Device& device) {
+    cl_int err;
+    std::vector<cl::Platform> platforms;
+    // Get all available platforms (e.g., Xilinx, Intel, AMD)
+    OCL_CHECK(err, err = cl::Platform::get(&platforms));
+
+    // Find Xilinx platform
+    for (size_t i = 0; i < platforms.size(); i++) {
+        cl::Platform platform = platforms[i];
+        std::string platformName = platform.getInfo<CL_PLATFORM_NAME>(&err);
+        if (platformName == "Xilinx") {
+  // Get devices for the Xilinx platform
+            OCL_CHECK(err, err = platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices));
+            device = devices[0]; // Use the first available device
+            return EXIT_SUCCESS;
+        }
+    }
+    std::cerr << "Error: Failed to find Xilinx platform or devices." << std::endl;
+    return EXIT_FAILURE;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -24,15 +47,22 @@ int main(int argc, char** argv) {
         inputData[i] = i % 256;
     }
 
-    // Get the Xilinx devices and program the device
-    auto devices = xcl::get_xil_devices();
-    auto device = devices[0];
+    // Set up OpenCL device
+    std::vector<cl::Device> devices;
+    cl::Device device;
+    if (setupDevice(devices, device) != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+ // Create OpenCL context and command queue
     auto context = cl::Context(device, nullptr, nullptr, nullptr, &err);
     auto q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
+
+    // Read binary file and create program
     auto fileBuf = xcl::read_binary_file(binaryFile);
     cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
     auto program = cl::Program(context, {device}, bins, nullptr, &err);
 
+    // Create kernel
     auto krnl = cl::Kernel(program, "processHits", &err);
 
     // Allocate Buffer in Global Memory

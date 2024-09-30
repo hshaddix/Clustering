2b05d06cd1f591bcdd7d1f410524c8d52837ac8c
hshaddix <hshaddix@niu.edu>
2024-09-30 20:12:41 +0200

    'Resolved merge conflicts'

--- tests/ABCStarApplication/Vitis_HLS_Kernel/src/host.cpp
+++ tests/ABCStarApplication/Vitis_HLS_Kernel/src/host.cpp
@@ -1,83 +1,91 @@
-#include "xcl2.hpp"
+#define CL_TARGET_OPENCL_VERSION 220  // Target OpenCL version 2.2
+#include <CL/cl.h>
 #include <vector>
 #include <iostream>
 #include <bitset>
+#include <fstream>
 
 // Define the data size
 #define DATA_SIZE 4096
 
-// Function to set up OpenCL device
-int setupDevice(std::vector<cl::Device>& devices, cl::Device& device) {
-    bool found_device = false;
-  // traversing all Platforms To find Xilinx Platform and targeted
-  // Device in Xilinx Platform
-  std::vector < cl::Platform > platforms;
-  cl::Platform::get( & platforms);
-  std::cout << "Scanning Platforms" << std::endl;
-  std::cout << "number: " << platforms.size() << std::endl;
-  for (size_t i = 0;
-    (i < platforms.size()) & (found_device == false); i++) {
-    cl::Platform platform = platforms[i];
-    std::string platformName = platform.getInfo < CL_PLATFORM_NAME > ();
-    std::cout << "Found platform: " << std::endl;
-    std::cout << platformName << std::endl;
-    if (platformName == "Xilinx") {
-      devices.clear();
-      platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, & devices);
-      std::cout << "number Devices: " << devices.size() << std::endl;
-      if (devices.size()) {
-        for (size_t d = 0; d < devices.size(); d++) {
-          std::string deviceName = devices[0].getInfo < CL_DEVICE_NAME > ();
-          std::cout << "Device: (" << d << "): " << deviceName << std::endl;
-          // if(deviceName.find("u280") != std::string::npos){
-          device = devices[0];
-          found_device = true;
-          // break;
-          // } else {
-          // devices.erase(devices.begin());
-          // }
-        }
-      }
+// Error checking macro
+#define checkError(status, message) \
+    if (status != CL_SUCCESS) { \
+        std::cerr << "Error: " << message << " (" << status << ")" << std::endl; \
+        exit(EXIT_FAILURE); \
     }
-  }
-  // return EXIT_FAILURE;
-  if (found_device == false) {
-    std::cout << "Error: Unable to find Target Device " <<
-      device.getInfo < CL_DEVICE_NAME > () << std::endl;
-    return EXIT_FAILURE;
-  }
-  devices.resize(1);
-  return 0;
 
+// Function to read binary file
+std::vector<unsigned char> readBinaryFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open binary file " << filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    std::vector<unsigned char> buffer(size);
+    if (!file.read(reinterpret_cast<char*>(buffer.data()), size)) {
+        std::cerr << "Error: Unable to read binary file " << filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return buffer;
+}
 
-    //cl_int err;
-    //std::vector<cl::Platform> platforms;
-    //// Get all available platforms (e.g., Xilinx, Intel, AMD)
-    //OCL_CHECK(err, err = cl::Platform::get(&platforms));
+// Function to set up OpenCL device
+int setupDevice(cl_device_id& device) {
+    cl_int status;
+    cl_uint numPlatforms;
+    status = clGetPlatformIDs(0, nullptr, &numPlatforms);
+    checkError(status, "Failed to get platform IDs.");
 
-    //// Find Xilinx platform
-    //for (size_t i = 0; i < platforms.size(); i++) {
-    //    cl::Platform platform = platforms[i];
-    //    std::string platformName = platform.getInfo<CL_PLATFORM_NAME>(&err);
-    //    if (platformName == "Xilinx") {
-  //// Get devices for the Xilinx platform
-    //        OCL_CHECK(err, err = platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices));
-    //        device = devices[0]; // Use the first available device
-    //        return EXIT_SUCCESS;
-    //    }
-    //}
-    //std::cerr << "Error: Failed to find Xilinx platform or devices." << std::endl;
-    //return EXIT_FAILURE;
+    std::vector<cl_platform_id> platforms(numPlatforms);
+    status = clGetPlatformIDs(numPlatforms, platforms.data(), nullptr);
+    checkError(status, "Failed to get platform IDs.");
+
+    bool found_device = false;
+    for (size_t i = 0; i < platforms.size() && !found_device; ++i) {
+        cl_platform_id platform = platforms[i];
+        char platformName[128];
+        status = clGetPlatformInfo(platform, CL_PLATFORM_NAME, 128, platformName, nullptr);
+        checkError(status, "Failed to get platform name.");
+
+        if (std::string(platformName) == "Xilinx") {
+            cl_uint numDevices;
+            status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ACCELERATOR, 0, nullptr, &numDevices);
+            checkError(status, "Failed to get device IDs.");
+
+            std::vector<cl_device_id> devices(numDevices);
+            status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ACCELERATOR, numDevices, devices.data(), nullptr);
+            checkError(status, "Failed to get device IDs.");
+
+            for (size_t d = 0; d < devices.size(); ++d) {
+                device = devices[d];
+                char deviceName[128];
+                status = clGetDeviceInfo(device, CL_DEVICE_NAME, 128, deviceName, nullptr);
+                checkError(status, "Failed to get device name.");
+                std::cout << "Found device: " << deviceName << std::endl;
+                found_device = true;
+                break;
+            }
+        }
+    }
+
+    if (!found_device) {
+        std::cerr << "Error: Unable to find target device" << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
         return EXIT_FAILURE;
     }
 
     std::string binaryFile = argv[1];
-    cl_int err;
+    cl_int status;
     std::vector<short> inputData(DATA_SIZE);
     std::vector<short> outputData(DATA_SIZE);
 
@@ -90,45 +98,83 @@ int main(int argc, char** argv) {
     }
 
     // Set up OpenCL device
-    std::vector<cl::Device> devices;
-    cl::Device device;
-    if (setupDevice(devices, device) != EXIT_SUCCESS) {
+    cl_device_id device;
+    if (setupDevice(device) != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
- // Create OpenCL context and command queue
-    auto context = cl::Context(device, nullptr, nullptr, nullptr, &err);
-    auto q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
+
+    // Create OpenCL context and command queue
+    cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &status);
+    checkError(status, "Failed to create context.");
+
+    cl_command_queue_properties props[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE, 0 };
+    cl_command_queue queue = clCreateCommandQueueWithProperties(context, device, props, &status);
+    checkError(status, "Failed to create command queue with properties.");
 
     // Read binary file and create program
-    auto fileBuf = xcl::read_binary_file(binaryFile);
-    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
-    auto program = cl::Program(context, {device}, bins, nullptr, &err);
+    std::vector<unsigned char> fileBuf = readBinaryFile(binaryFile);
+
+    size_t binary_size = fileBuf.size();
+    if (binary_size == 0) {
+        std::cerr << "Error: Binary file is empty or could not be read." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const unsigned char* binary_data = fileBuf.data();
+    cl_program program = clCreateProgramWithBinary(context, 1, &device, &binary_size, &binary_data, nullptr, &status);
+    if (status != CL_SUCCESS || program == nullptr) {
+        std::cerr << "Failed to create program with binary. Error code: " << status << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    status = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
+    checkError(status, "Failed to build program.");
 
     // Create kernel
-    auto krnl = cl::Kernel(program, "processHits", &err);
+    cl_kernel kernel = clCreateKernel(program, "processHits", &status);
+    checkError(status, "Failed to create kernel.");
 
     // Allocate Buffer in Global Memory
-    cl::Buffer buffer_in(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, input_size_in_bytes, inputData.data(), &err);
-    cl::Buffer buffer_out(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, output_size_in_bytes, outputData.data(), &err);
+    cl_mem buffer_in = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, input_size_in_bytes, inputData.data(), &status);
+    checkError(status, "Failed to create input buffer.");
+
+    cl_mem buffer_out = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, output_size_in_bytes, outputData.data(), &status);
+    checkError(status, "Failed to create output buffer.");
 
     // Set the kernel arguments
-    krnl.setArg(0, buffer_in);
-    krnl.setArg(1, buffer_out);
+    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &buffer_in);
+    checkError(status, "Failed to set kernel argument 0.");
+
+    status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &buffer_out);
+    checkError(status, "Failed to set kernel argument 1.");
 
     // Copy input data to device global memory
-    q.enqueueMigrateMemObjects({buffer_in}, 0);
+    status = clEnqueueWriteBuffer(queue, buffer_in, CL_TRUE, 0, input_size_in_bytes, inputData.data(), 0, nullptr, nullptr);
+    checkError(status, "Failed to enqueue write buffer.");
 
     // Launch the Kernel
-    q.enqueueTask(krnl);
+    size_t globalWorkSize = DATA_SIZE;
+    status = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &globalWorkSize, nullptr, 0, nullptr, nullptr);
+    checkError(status, "Failed to enqueue ND range kernel.");
 
     // Copy result from device global memory to host local memory
-    q.enqueueMigrateMemObjects({buffer_out}, CL_MIGRATE_MEM_OBJECT_HOST);
-    q.finish();
+    status = clEnqueueReadBuffer(queue, buffer_out, CL_TRUE, 0, output_size_in_bytes, outputData.data(), 0, nullptr, nullptr);
+    checkError(status, "Failed to enqueue read buffer.");
+    clFinish(queue);
 
     // Print output
     for (size_t i = 0; i < DATA_SIZE; i++) {
         std::cout << "Output[" << i << "] = " << std::bitset<16>(outputData[i]) << std::endl;
     }
 
+    // Cleanup
+    clReleaseMemObject(buffer_in);
+    clReleaseMemObject(buffer_out);
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+
     return 0;
 }
+
